0a2e7a7a23ac706102f6f1943210daf2e65a058c
rkoponen <ruupert.koponen@gmail.com>
2023-06-12 18:29:36 +0300

    Edit recipe descriptions

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -10,12 +10,12 @@ enum RecipeCategory {
 }
 
 Map<RecipeCategory, String> categoryDescriptions = {
-  RecipeCategory.breakfast: 'Start your day with delicious breakfast recipes.',
-  RecipeCategory.salad: 'Fresh and healthy salad recipes for a light meal or side dish.',
-  RecipeCategory.soup: 'Warm and comforting soup recipes to satisfy your taste buds.',
-  RecipeCategory.dessert: 'Indulgent dessert recipes to satisfy your sweet tooth.',
-  RecipeCategory.main: 'Delicious and hearty main course recipes for any occasion.',
-  RecipeCategory.vegetarian: 'Wholesome vegetarian recipes packed with flavor.',
-  RecipeCategory.vegan: 'Plant-based vegan recipes for a compassionate and healthy diet.',
-  RecipeCategory.side: 'Tasty side dish recipes to complement your meals.',
+  RecipeCategory.breakfast: 'Delicious breakfast recipes to start your day.',
+  RecipeCategory.salad: 'Fresh and healthy salad recipes.',
+  RecipeCategory.soup: 'Flavorful soup recipes.',
+  RecipeCategory.dessert: 'Sweet treats for any occasion.',
+  RecipeCategory.main: 'Tasty dishes for your main course.',
+  RecipeCategory.vegetarian: 'Vegetarian recipes packed with flavor.',
+  RecipeCategory.vegan: 'Plant-based vegan recipes for a healthy diet.',
+  RecipeCategory.side: 'Yummy side dish recipes to complement your meals.',
 };
