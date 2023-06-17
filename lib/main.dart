5cef6022e30abd7f68aa5f7cf159773204854cc6
rkoponen <ruupert.koponen@gmail.com>
2023-06-17 18:39:19 +0300

    Revert "Take provider to use"
    This reverts commit 52b8bde02079fb278a8bded50fbd2079ebfbef71.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:recipes_app/routes/router.dart';
 import 'package:recipes_app/screens/home_screen.dart';
 import 'firebase_options.dart';
 
@@ -13,9 +12,7 @@ void main() async {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final snapshot = await _firestore.collection('recipes').get();
   snapshot.docs.forEach((doc) => print('${doc.id}: ${doc.data()}'));
-  runApp(ProviderScope(
-      child: MaterialApp.router(
-          routerConfig: router, theme: ThemeData(useMaterial3: true))));
+  runApp(const ProviderScope(child: RecipeApp()));
 }
 
 class RecipeApp extends StatelessWidget {
