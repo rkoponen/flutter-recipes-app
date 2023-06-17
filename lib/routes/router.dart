3d051fef51240ee08e9856847b4ca660566eca5d
rkoponen <ruupert.koponen@gmail.com>
2023-06-17 18:37:25 +0300

    Add router

---
+++ lib/routes/router.dart
@@ -0,0 +1,25 @@
+import 'dart:js';
+
+import 'package:go_router/go_router.dart';
+import 'package:recipes_app/screens/home_screen.dart';
+import 'package:recipes_app/screens/recipe_screen.dart';
+import 'package:recipes_app/widgets/scaffold_with_bottom_nav.dart';
+
+final router = GoRouter(initialLocation: '/', routes: [
+  ShellRoute(
+      builder: (context, state, child) {
+        return ScaffoldWithBottomNav(child: child);
+      },
+      routes: [
+        GoRoute(
+            path: '/',
+            builder: (context, state) {
+              return HomeScreen();
+            }),
+        GoRoute(
+          path: '/recipe/:recipeId',
+          builder: (context, state) =>
+              RecipeScreen(recipeId: state.pathParameters['recipeId']!),
+        )
+      ])
+]);
