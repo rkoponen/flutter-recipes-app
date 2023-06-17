52b8bde02079fb278a8bded50fbd2079ebfbef71
rkoponen <ruupert.koponen@gmail.com>
2023-06-17 18:38:00 +0300

    Take provider to use

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipes_app/routes/router.dart';
 import 'package:recipes_app/screens/home_screen.dart';
 import 'firebase_options.dart';
 
@@ -12,7 +13,9 @@ void main() async {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final snapshot = await _firestore.collection('recipes').get();
   snapshot.docs.forEach((doc) => print('${doc.id}: ${doc.data()}'));
-  runApp(const ProviderScope(child: RecipeApp()));
+  runApp(ProviderScope(
+      child: MaterialApp.router(
+          routerConfig: router, theme: ThemeData(useMaterial3: true))));
 }
 
 class RecipeApp extends StatelessWidget {
