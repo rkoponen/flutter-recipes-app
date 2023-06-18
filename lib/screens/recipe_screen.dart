092a4dd0c383bfef4bdba4daeb7b7868091b038a
rkoponen <ruupert.koponen@gmail.com>
2023-06-18 11:37:42 +0300

    Add recipe screen to display a single recipe

---
+++ lib/screens/recipe_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipes_app/providers/recipe_provider.dart';
+import 'package:recipes_app/widgets/ingredient_list_widget.dart';
+import 'package:recipes_app/widgets/steps_list_widget.dart';
+
+class RecipeScreen extends ConsumerWidget {
+  String recipeId;
+
+  RecipeScreen({required this.recipeId});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final recipe = ref.watch(recipeProvider.notifier).findRecipeById(recipeId);
+    //categoriesStr += recipe.categories.last;
+    return SingleChildScrollView(
+        child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Categories: ${recipe.categories.join(", ")}",
+                style: const TextStyle(
+                    fontStyle: FontStyle.italic, fontWeight: FontWeight.w300),
+              )),
+          const SizedBox(height: 16),
+          const Placeholder(
+            fallbackHeight: 200,
+            fallbackWidth: 200,
+          ),
+          ListTile(
+            title: Text(recipe.name,
+                style: Theme.of(context).textTheme.headlineLarge),
+            subtitle: Text(recipe.description,
+                style: Theme.of(context).textTheme.titleMedium),
+            trailing: const Icon(Icons.favorite_border_rounded, size: 30.0),
+          ),
+          const Divider(),
+          Text("Ingredients",
+              style: Theme.of(context).textTheme.headlineSmall,
+              textAlign: TextAlign.left),
+          IngredientListWidget(ingredients: recipe.ingredients),
+          const Divider(),
+          Text("Steps", style: Theme.of(context).textTheme.headlineSmall),
+          StepsListWidget(steps: recipe.steps)
+        ],
+      ),
+    ));
+  }
+}
