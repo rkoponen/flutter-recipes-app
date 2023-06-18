55c312d07080d6e0eb989ff98ed4579f858a435e
rkoponen <ruupert.koponen@gmail.com>
2023-06-18 11:42:13 +0300

    Add a scaffold widget

---
+++ lib/widgets/scaffold_with_bottom_nav.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'bottom_nav_bar.dart';
+
+class ScaffoldWithBottomNav extends StatelessWidget {
+  Widget child;
+  ScaffoldWithBottomNav({super.key, 
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColorLight,
+          title: const Text("Recipes"),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () => print("Search pressed."),
+            )
+          ],
+        ),
+        body: child,
+        bottomNavigationBar: const BottomNavBar());
+  }
+}
