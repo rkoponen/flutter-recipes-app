0f8856e22f9dca56c762eda8c4c10479992ce170
rkoponen <ruupert.koponen@gmail.com>
2023-07-02 18:15:49 +0300

    Remove unneccessary import and add 'const'

--- lib/screens/recipe_list_screen.dart
+++ lib/screens/recipe_list_screen.dart
@@ -3,19 +3,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recipes_app/widgets/recipe_list_widget.dart';
 
 class RecipeListScreen extends ConsumerWidget {
-  String categoryName;
+  final String categoryName;
 
-  RecipeListScreen({required this.categoryName, super.key});
+  const RecipeListScreen({required this.categoryName, super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: implement build
     return Column(
       children: [
-        SizedBox(height: 16),
-        Text("Category: $categoryName",
-            style: Theme.of(context).textTheme.titleLarge),
-        SizedBox(height: 16),
+        const SizedBox(height: 16),
+        Row(
+          children: [
+            const BackButton(),
+            const SizedBox(width: 16),
+            Text("Category: $categoryName",
+                style: Theme.of(context).textTheme.titleLarge),
+          ],
+        ),
+        const SizedBox(height: 16),
         RecipeListWidget(categoryName: categoryName)
       ],
     );
