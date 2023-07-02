6f4b90af4550a8bdc29b45919d45626d06c26398
rkoponen <ruupert.koponen@gmail.com>
2023-07-02 18:19:00 +0300

    Refactor bottom nav bar to use Provider

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,27 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:recipes_app/providers/recipe_provider.dart';
 
 final indexProvider = StateProvider<int>((ref) => 0);
 
-class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({super.key});
-
+class BottomNavBar extends ConsumerWidget {
   @override
-  State<BottomNavBar> createState() => BottomNavBarState();
-}
-
-class BottomNavBarState extends State<BottomNavBar> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var selectedIndex = ref.watch(indexProvider);
     return BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
@@ -30,15 +17,17 @@ class BottomNavBarState extends State<BottomNavBar> {
           BottomNavigationBarItem(
               icon: Icon(Icons.casino), label: 'Random recipe'),
         ],
-        currentIndex: _selectedIndex,
+        currentIndex: ref.watch(indexProvider),
         onTap: (index) {
-          _onItemTapped(index);
-          if (_selectedIndex == 0) {
-            context.go("/");
-          } else if (_selectedIndex == 1) {
-            context.go("/categories");
-          } else if (_selectedIndex == 2) {
-            context.go("/random");
+          ref.read(indexProvider.notifier).state = index;
+          selectedIndex = ref.watch(indexProvider);
+          if (selectedIndex == 0) {
+            context.push("/");
+          } else if (selectedIndex == 1) {
+            context.push("/category");
+          } else if (selectedIndex == 2) {
+            context.push(
+                "/recipe/random/${ref.watch(recipeProvider.notifier).getRandomRecipe().id}");
           }
         });
   }
