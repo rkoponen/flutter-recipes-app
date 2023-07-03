6be30343a215b0e09b1eb388b31bf7897bc13922
rkoponen <ruupert.koponen@gmail.com>
2023-07-03 13:11:40 +0300

    Add ensureInitalized

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:recipes_app/screens/home_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
