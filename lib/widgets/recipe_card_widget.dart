758594f866b8bfab22bc61247deba5c294cf2964
rkoponen <ruupert.koponen@gmail.com>
2023-07-03 13:13:00 +0300

    Add controller parameter

--- lib/widgets/recipe_card_widget.dart
+++ lib/widgets/recipe_card_widget.dart
@@ -5,29 +5,36 @@ import '../models/recipe.dart';
 
 class RecipeCardWidget extends StatelessWidget {
   final Recipe recipe;
-  const RecipeCardWidget({super.key, required this.recipe});
+  final SearchController? controller;
+  const RecipeCardWidget(
+      {super.key, required this.recipe, this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: SizedBox(
-            width: MediaQuery.of(context).size.width - 50,
-            height: 150,
-            child: Column(children: [
-              Expanded(
-                child: SizedBox(
-                    child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10.0),
-                            child: const Placeholder()))),
-              ),
-              ListTile(
-                onTap: () => context.push('/recipe/${recipe.id}'),
-                title: Text(recipe.name),
-                subtitle: Text(recipe.description),
-                trailing: const Icon(Icons.favorite_border_rounded),
-              )
-            ])));
+    return GestureDetector(
+        child: Card(
+          child: SizedBox(
+              width: MediaQuery.of(context).size.width - 50,
+              height: 150,
+              child: Column(children: [
+                Expanded(
+                  child: SizedBox(
+                      child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: const Placeholder()))),
+                ),
+                ListTile(
+                  title: Text(recipe.name),
+                  subtitle: Text(recipe.description),
+                  trailing: const Icon(Icons.favorite_border_rounded),
+                )
+              ])),
+        ),
+        onTap: () {
+          context.push('/recipe/${recipe.id}');
+          if (controller != null) controller!.closeView("");
+        });
   }
 }
