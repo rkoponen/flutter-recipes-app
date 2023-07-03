548eb99333d6fd0d229afecaf9b39073ecd251c3
rkoponen <ruupert.koponen@gmail.com>
2023-07-03 13:09:13 +0300

    Add padding around the widget

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -3,17 +3,27 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recipes_app/widgets/category_list_widget.dart';
 
 class CategoryScreen extends ConsumerWidget {
-  const CategoryScreen({super.key});
+  final bool backButton;
+
+  const CategoryScreen({required this.backButton, super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return SingleChildScrollView(
-        child: Column(
-      children: [
-        const SizedBox(height: 16),
-        Text("Categories", style: Theme.of(context).textTheme.titleLarge),
-        const CategoryListWidget(subset: false)
-      ],
-    ));
+        child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    if (backButton) BackButton(),
+                    Text("Categories",
+                        style: Theme.of(context).textTheme.titleLarge),
+                  ],
+                ),
+                SizedBox(height: 16),
+                const Center(child: CategoryListWidget(subset: false)),
+              ],
+            )));
   }
 }
