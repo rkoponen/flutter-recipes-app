5717123b0228f0a6e1a96681e2adb28cdb6170dd
rkoponen <ruupert.koponen@gmail.com>
2023-07-03 13:14:03 +0300

    Add search bar to the app bar

--- lib/widgets/scaffold_with_bottom_nav.dart
+++ lib/widgets/scaffold_with_bottom_nav.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recipes_app/widgets/search_bar_widget.dart';
 
 import 'bottom_nav_bar.dart';
 
-class ScaffoldWithBottomNav extends StatelessWidget {
+class ScaffoldWithBottomNav extends ConsumerWidget {
   final Widget child;
-  const ScaffoldWithBottomNav({
-    super.key,
-    required this.child
-  });
+  const ScaffoldWithBottomNav({super.key, required this.child});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).primaryColorLight,
-          title: const Text("Recipes App"),
+          leading: Icon(Icons.soup_kitchen),
           actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.search),
-              onPressed: () => print("Search pressed."),
-            )
+            SearchBarWidget()
           ],
         ),
         body: child,
-        bottomNavigationBar: const BottomNavBar());
+        bottomNavigationBar: BottomNavBar());
   }
 }
