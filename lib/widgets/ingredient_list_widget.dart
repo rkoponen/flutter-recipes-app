7a6e08ad0ba70e1d2eae783f5e2e99ad7fda1ea2
rkoponen <ruupert.koponen@gmail.com>
2023-07-04 11:47:21 +0300

    Wrap the listviews inside a card

--- lib/widgets/ingredient_list_widget.dart
+++ lib/widgets/ingredient_list_widget.dart
@@ -8,16 +8,20 @@ class IngredientListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: ingredients.length,
-      itemBuilder: (BuildContext context, int index) {
-        final ingredient = ingredients[index];
-        return Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Text("\u2022 ${ingredient.name}: ${ingredient.amount}",
-                style: Theme.of(context).textTheme.bodyLarge));
-      },
-    );
+    return Card(
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: ingredients.length,
+              itemBuilder: (BuildContext context, int index) {
+                final ingredient = ingredients[index];
+                return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Text(
+                        "\u2022 ${ingredient.name}: ${ingredient.amount}",
+                        style: Theme.of(context).textTheme.bodyLarge));
+              },
+            )));
   }
 }
--- lib/widgets/steps_list_widget.dart
+++ lib/widgets/steps_list_widget.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
 
-
 class StepsListWidget extends StatelessWidget {
   final List<String> steps;
   const StepsListWidget({super.key, required this.steps});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: steps.length,
-      itemBuilder: (BuildContext context, int index) {
-        final step = steps[index];
-        return Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: RichText(
-                text: TextSpan(
-                    style: Theme.of(context).textTheme.bodyLarge,
-                    children: <TextSpan>[
-                  TextSpan(
-                      text: "${index + 1}: ",
-                      style: const TextStyle(fontWeight: FontWeight.bold)),
-                  TextSpan(text: step)
-                ])));
-      },
-    );
+    return Card(
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: steps.length,
+              itemBuilder: (BuildContext context, int index) {
+                final step = steps[index];
+                return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: RichText(
+                        text: TextSpan(
+                            style: Theme.of(context).textTheme.bodyLarge,
+                            children: <TextSpan>[
+                          TextSpan(
+                              text: "${index + 1}: ",
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          TextSpan(text: step)
+                        ])));
+              },
+            )));
   }
 }
