47d4d45f82f2866ab1fec5d7dc71e35a8c5c70d2
rkoponen <ruupert.koponen@gmail.com>
2023-07-08 19:22:10 +0300

    Add padding and change text styles

--- lib/widgets/big_recipe_card_widget.dart
+++ lib/widgets/big_recipe_card_widget.dart
@@ -2,30 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:recipes_app/widgets/ingredient_list_widget.dart';
 import 'package:recipes_app/widgets/steps_list_widget.dart';
 
-import '../models/ingredient.dart';
 import '../models/recipe.dart';
 
 class BigRecipeCardWidget extends StatelessWidget {
   final Recipe featuredRecipe;
 
-  BigRecipeCardWidget({required this.featuredRecipe});
-
+  const BigRecipeCardWidget({super.key, required this.featuredRecipe});
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
 
     return Expanded(
         child: Padding(
-            padding: EdgeInsets.all(30.0),
+            padding: const EdgeInsets.all(20.0),
             child: Card(
                 child: ListView(children: [
               Column(
                 children: [
-                  Text(featuredRecipe.name),
-                  Text(featuredRecipe.description),
-                  SizedBox(height: 300, width: 400, child: Placeholder()),
+                  const SizedBox(height: 5),
+                  Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                          child: Column(
+                        children: [
+                          Text("Featured recipe: ${featuredRecipe.name}",
+                              style: Theme.of(context).textTheme.titleLarge),
+                          Text(featuredRecipe.description,
+                              style: const TextStyle(fontStyle: FontStyle.italic)),
+                        ],
+                      ))),
+                  const SizedBox(height: 300, width: 400, child: Placeholder()),
+                  const SizedBox(height: 10),
+                  Text("Ingredients",
+                      style: Theme.of(context).textTheme.titleMedium),
                   IngredientListWidget(ingredients: featuredRecipe.ingredients),
+                  const SizedBox(height: 10),
+                  Text("Steps", style: Theme.of(context).textTheme.titleMedium),
                   StepsListWidget(steps: featuredRecipe.steps)
                 ],
               )
