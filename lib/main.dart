774086cc9a3107d6e35a6dc20d73d90910838063
rkoponen <ruupert.koponen@gmail.com>
2023-07-08 19:28:38 +0300

    Remove unnecessary prints

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:recipes_app/routes/router.dart';
-import 'package:recipes_app/screens/home_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -11,18 +9,14 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-  final snapshot = await firestore.collection('recipes').get();
-  for (var doc in snapshot.docs) {
-    print('${doc.id}: ${doc.data()}');
-  }
   runApp(ProviderScope(
       child: MaterialApp.router(
           routerConfig: router,
           theme: ThemeData(
+              primaryColor: const Color.fromARGB(255, 255, 197, 194),
               fontFamily: 'Noto Serif',
               useMaterial3: true,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
               scaffoldBackgroundColor:
                   const Color.fromARGB(255, 255, 255, 255)))));
 }
